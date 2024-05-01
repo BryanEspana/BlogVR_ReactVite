17eec320c71d7b428ca6fd0753cc9760e0d120e5
Bryan España <bryanespana21550@gmail.com>
2024-04-30 19:16:56 -0600

    Routes of topbar

--- src/components/topbar.tsx
+++ src/components/topbar.tsx
@@ -1,13 +1,26 @@
 import React from "react";
 
 function TopBar(){
+
+    const goHome = (event) => {
+        console.log("Go Home");
+        window.location.href = '/';
+    }
+    const goAboutMe = (event) => {
+        console.log("Go Home");
+        window.location.href = '/';
+    }
+    const goLogin = (event) => {
+        console.log("Go Home");
+        window.location.href = '/login';
+    }
     return (
         <div className="Topbar">
             <img src="src/assets/logo.png" className="logo" />
             <div>
-                <button className="BtnToopbar">Home</button>
-                <button className="BtnToopbar">About Me</button>
-                <button className="BtnToopbar">Login</button>
+                <button className="BtnToopbar" onClick={goHome}>Home</button>
+                <button className="BtnToopbar" onClick={goAboutMe}>About Me</button>
+                <button className="BtnToopbar" onClick={goLogin}>Login</button>
             </div>
         </div>
     )
